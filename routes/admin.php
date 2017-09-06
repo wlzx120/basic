f959af84b5ca7384455d542d80cf7eb89cd003c9
wlzx120 <wlzx120@163.com>
2017-09-06 14:06:44 +0800

    finish admin-home-route-ueditor

--- routes/admin.php
+++ routes/admin.php
@@ -11,29 +11,21 @@ use Illuminate\Http\Request;
 
 //登录退出
 Route::get('/','IndexController@index')->name('admin');
-Route::get('/login','SessionController@login')->name('admin.login');
-Route::post('/login','SessionController@store')->name('admin.login');
-Route::delete('/logout','SessionController@logout')->name('admin.logout');
+Route::get('login','SessionController@login')->name('admin.login');
+Route::post('login','SessionController@store')->name('admin.login');
+Route::delete('logout','SessionController@logout')->name('admin.logout');
 
 //验证码
-Route::get('/yzm', 'IndexController@captcha')->name('admin.yzm');
+Route::get('yzm', 'IndexController@captcha')->name('admin.yzm');
 
 //栏目管理
-Route::get('column/index','ColumnController@index')->name('admin.column.index');
-Route::get('column/{column}/edit','ColumnController@edit')->name('admin.column.edit');
-Route::patch('column/{column}','ColumnController@update')->name('admin.column.update');
-Route::get('column/create','ColumnController@create')->name('admin.column.create');
-Route::post('column/store','ColumnController@store')->name('admin.column.store');
-Route::delete('column/{column}','ColumnController@destroy')->name('admin.column.destroy');
+Route::resource('column','ColumnController',['as' => 'admin']);
+
 //文章管理
-Route::get('article/index','ArticleController@index')->name('admin.article.index');
-Route::get('article/{article}','ArticleController@show')->name('admin.article.show');
-Route::get('article/{article}/edit','ArticleController@edit')->name('admin.article.edit');
-Route::patch('article/{article}','ArticleController@update')->name('admin.article.update');
-Route::get('article/create','ArticleController@create')->name('admin.article.create');
-Route::post('article/store','ArticleController@store')->name('admin.article.store');
-Route::delete('article/{article}','ArticleController@destroy')->name('admin.article.destroy');
+Route::resource('article','ArticleController',['as' => 'admin']);
+Route::post('article/create','ArticleController@store')->name('admin.article.store');
+
 //文章搜索
-Route::post('article/index','ArticleController@index')->name('admin.article.index');
+Route::post('article','ArticleController@index')->name('admin.article.index');
 
 
